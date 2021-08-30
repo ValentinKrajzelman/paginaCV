7dad8c850542a925f3d5606f33867232a0d09369
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2021-08-30 02:03:47 -0300

    se sigue

--- src/components/habilidades.component.js
+++ src/components/habilidades.component.js
@@ -8,8 +8,8 @@ const Habilidades = () => {
         <div id="texto1del02">Programador junior Full-stack. Tengo un conocimiento general del stack MERN
          (MongoDB, Express, React, Node.js), aprendo y me adapto rápido.</div>
          
-        <div id="texto2del02">Mi fuerte es: (ponerlas en letra un toque mas grande y con icono)
-        JavaScript y el stack MERN: MongoDB Express  React.js  Node.js</div>
+        <div id="texto2del02">Mi fuerte es: 
+        JavaScript  MERN: MongoDB Express  React.js  Node.js</div>
         
         <div id="texto3del02">Tambien utilizo o conozco:
         Postgres mysql html css sass
