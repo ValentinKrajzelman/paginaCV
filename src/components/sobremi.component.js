f9600874351f62cf692b9c579ba1731c6ca42fa6
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2021-08-31 07:24:03 -0300

    se sigue

--- src/components/sobremi.component.js
+++ src/components/sobremi.component.js
@@ -8,15 +8,13 @@ const Sobremi = () => {
         <div id="texto1del03">Mi nombre es Valentin Krajzelman hice tres semestres de la tecnicatura en desarrollo de la UCES,
          llevo unos tres años programando y seis meses trabajando en trenes argentinos como desarrollador.</div>
 
+        <div id="texto2del03">Trabajar en solitario es lento y difícil, quiero entrar en un equipo que quiera 
+        invertir en mi y donde pueda colaborar con compañeros.</div>
+
         <div id="texto3del03">Tengo un proyecto hosteado es un bugtracker si te interesa
          verlo hace click aca.</div>
 
         <div id="texto4del03">Para descargar mi CV con mis datos personales hacer click acá.</div>
-
-        <div id="texto2del03">Trabajar en solitario es lento y difícil, quiero entrar en un equipo que quiera 
-        invertir en mi y a cambio yo les pueda ofrecer mi esfuerzo.</div>
-
-
 
         <div className="lineahoja">a</div>
         <div className="piehoja">.03</div>
