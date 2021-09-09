f35e30c56dad1dbc50a5d98ee69c75707b687d9d
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2021-09-09 13:21:00 -0300

    a ver

--- src/components/habilidades.component.js
+++ src/components/habilidades.component.js
@@ -5,7 +5,7 @@ const Habilidades = () => {
 
     
         return(<div className="contenido">
-        <div id="texto1del02">Programador junior Full-stack. Tengo un conocimiento general del stack MERN
+        <div id="texto1del02">Programador junior Full-Stack. Tengo un conocimiento general del stack MERN
          (MongoDB, Express, React, Node.js), aprendo y me adapto rapido.</div>
          
         <div id="texto2del02">Mi fuerte es: 
