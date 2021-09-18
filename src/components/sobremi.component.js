83fb04cb9a322c852dfe297377f31fe1519bd15f
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2021-09-17 22:51:02 -0300

    cambie un par de cosas

--- src/components/sobremi.component.js
+++ src/components/sobremi.component.js
@@ -8,7 +8,7 @@ const Sobremi = () => {
         <div id="texto1del03">Mi nombre es Valentin Krajzelman hice tres semestres de la tecnicatura en desarrollo de la UCES,
          llevo unos tres años programando y seis meses trabajando en trenes argentinos como desarrollador.</div>
 
-        <div id="texto2del03">Trabajar en solitario es lento y difícil, quiero entrar en un equipo que quiera 
+        <div id="texto2del03">No me gusta trabajar en solitario, quiero entrar en un equipo que quiera 
         invertir en mi y donde pueda colaborar con compañeros.</div>
 
         <div id="texto3del03">Tengo un proyecto hosteado es un bugtracker si te interesa
