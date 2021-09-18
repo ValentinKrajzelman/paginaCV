5627cedab44a5d6e1346621c704a7418000747d8
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2021-09-18 00:34:41 -0300

    asdf

--- src/components/habilidades.component.js
+++ src/components/habilidades.component.js
@@ -11,9 +11,9 @@ const Habilidades = () => {
         <div id="texto2del02">Mi fuerte es: 
         JavaScript  MERN: MongoDB Express  React  Node</div>
         
-        <div id="texto3del02">Tambien utilizo o conozco:
+        <div id="texto3del02">Tambien utilizo o conozco: 
         Postgres mysql bootstrap mongoose
-         html css sass</div>
+         html css sass grid-box  </div>
         
         <div className="lineahoja">a</div>
         <div className="piehoja">.02</div>
